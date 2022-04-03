47d2f99f139886e4d000abfdb6c3ebe647e61495
Ajay Chava <cloudfreak@Ajays-MBP.lan>
2022-04-03 00:37:56 -0500

    Adding Scripts for test

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# setup.sh:  run the workstation setupbox
+#
+
+# Fail immediately if any errors occur
+set -e
+
+echo "Caching password..."
+sudo -K
+sudo true;
+clear
+
+MY_DIR="$(dirname "$0")"
+
+# Note: Homebrew needs to be set up first
+source ${MY_DIR}/scripts/common/homebrew.sh
+
+# Install everything else
+source ${MY_DIR}/scripts/common/oh-my-zsh.sh
+source ${MY_DIR}/scripts/common/editors.sh
+source ${MY_DIR}/scripts/common/git.sh
+source ${MY_DIR}/scripts/common/git-aliases.sh
+source ${MY_DIR}/scripts/common/applications-common.sh
+source ${MY_DIR}/scripts/common/developer-utilities.sh
+source ${MY_DIR}/scripts/common/unix.sh
+source ${MY_DIR}/scripts/common/configuration-osx.sh
